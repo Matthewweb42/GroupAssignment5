8f25d353cb7e6283e4a8b5d98106e2f7e4b3957e
Matthew Webecke <Matthew@webecke.com>
2025-03-25 13:07:32 -0600

    Person 3 stuff done

--- ProductInventory.java
+++ ProductInventory.java
@@ -11,10 +11,34 @@ public class ProductInventory {
     public void sortInventory(){
 
     }
-    public void addProduct(String productName, String locator, int quantity, float price){
 
+    //Matthew Webecke's Code
+    public void addProduct(String productName, String locator, int quantity, float price){
+        if (quantity < 0 || price < 0){
+            throw new ProductException("Invalid quantity or price");
+        }
+        ProductNode newNode = new ProductNode(productName, locator, quantity, price);
+        ProductNode current = inventoryHead;
+        while (current.next != null){
+            if(current.next.productName.equals(productName) && current.next.locator.equals(locator)){
+                throw new ProductException("Product already exists");
+            }
+            current = current.next;
+        }
+        current.next = newNode;
     }
+
+    //Matthew Webecke's Code
     public void removeProduct(String productName, String locator){
+        ProductNode current = inventoryHead;
+        while (current.next != null){
+            if (current.next.productName.equals(productName) && current.next.locator.equals(locator)){
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+        throw new ProductException("Product not found");
 
     }
     public int countProduct(String productName){
--- ProductNode.java
+++ ProductNode.java
@@ -1,9 +1,27 @@
+//Matthew Webecke's Code
 public class ProductNode {
+    String productName;
+    String locator;
+    int quantity;
+    float price;
     ProductNode next;
     //Constructor # 1
     ProductNode(){
+        productName = "";
+        locator = "";
+        quantity = 0;
+        price = 0.0f;
     // Intialize other variables here
         next= null;
     }
     //Constructor # 2
+    ProductNode(String productName, String locator, int quantity, float price){
+        this.productName = productName;
+        this.locator = locator;
+        this.quantity = quantity;
+        this.price = price;
+        this.next = null;
+    }
+
+    
 }
